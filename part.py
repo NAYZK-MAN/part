b9fa4132fc65a6aa8f206824173e61524654c33b
NAYZK-MAN <117479928+NAYZK-MAN@users.noreply.github.com>
2023-05-10 19:46:22 +0300

    Add files via upload

---
+++ part.py
@@ -0,0 +1,48 @@
+import os,sys
+os.system('clear')
+print('''\033[1;32m
+▐▓█▀▀▀▀▀▀▀▀▀█▓▌░▄▄▄▄▄░
+▐▓█░░▀░░▀▄░░█▓▌░█▄▄▄█░
+▐▓█░░▄░░▄▀░░█▓▌░█▄▄▄█░
+▐▓█▄▄▄▄▄▄▄▄▄█▓▌░█████░
+░░░░▄▄███▄▄░░░░░█████░
+''')
+print('''\033[1;32m
+██████╗░░█████╗░██████╗░████████╗
+██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝
+██████╔╝███████║██████╔╝░░░██║░░░
+██╔═══╝░██╔══██║██╔══██╗░░░██║░░░
+██║░░░░░██║░░██║██║░░██║░░░██║░░░
+╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░
+''')
+print('''\033[1;32m
+------------------------[~~~~~~~~~~~~~~~]------------------
+
+                          [  WELCOME  ]
+                          
+github : https://github.com/NAYZK-MAN
+
+------------------------[~~~~~~~~~~~~~~~]------------------
+''')
+print('')
+x = input('\033[1;32mEnter => : ')
+os.system('clear')
+os.system('apt update;apt upgrade;pkg update;pkg upgrade;pkg install wget -y')
+os.system('pkg install perl')
+os.system('pkg install ruby')
+os.system('pkg install proot')
+os.system('gem install resolved')
+os.system('bundle install rake-12.3.1')
+os.system('bundle config')
+os.system('build.nokogiri--use-system--libraries')
+os.system('gem install nokogiri')
+os.system('gem install pkg-config -v rake-12.3.1')
+os.system('bundle install rake-12.3.1')
+os.system('pkg update')
+os.system('pkg upgrade')
+os.system('wget https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh')
+os.system('chmod +x metasploit.sh')
+os.system('./metasploit.sh')
+os.system('clear')
+print('\033[1;32mwait')
+os.system('msfconsole')
